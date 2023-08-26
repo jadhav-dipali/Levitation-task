0969ad0c50d6d483353835a76060ca8482915532
jadhav-dipali <jadhavdipali924@gmail.com>
2023-08-26 21:58:19 +0530

    submitted some changes

--- Forntend/src/component/LoginUser.js
+++ Forntend/src/component/LoginUser.js
@@ -28,7 +28,7 @@ export default function LoginUser(){
        setPassErr(true)
     }else{
         setLoder(true)
-        fetch("http://localhost:4000/user/login",{
+        fetch("https://levitation-task-backend.onrender.com/user/login",{
             method:"POST",
             headers:{
              "content-type":"application/json"
